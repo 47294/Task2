9955f2c1c325302fdd03b6ffdccb6629a1c5a036
Artem <45859415+47294@users.noreply.github.com>
2021-10-06 14:50:55 +0300

    Update Logarithm.tests.cpp

---
+++ tests/Logarithm.tests.cpp
@@ -0,0 +1,12 @@
+#include "../include/Logarithm.h"
+
+#include <gtest/gtest.h>
+TEST (CheckingLog2, eight){
+	ASSERT_DOUBLE_EQ(log2(8), 3);
+}
+TEST (CheckingLog2, nine){
+	ASSERT_DOUBLE_EQ(log2(8), -2);
+}
+TEST (CheckingLog2, zero){
+	ASSERT_DOUBLE_EQ(result(log2(8), -1;
+}
