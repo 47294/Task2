4b3a52bb82ea79883fac5e92ac9139627bee6310
Artem <45859415+47294@users.noreply.github.com>
2021-10-06 14:47:27 +0300

    Function did

---
+++ src/Logarithm.cpp
@@ -0,0 +1,18 @@
+#include "../include/Logarithm.h"
+#include <math.h>
+int Log2(int x)
+{
+  logar = log(x) / log(2)
+  if (logar == int logar) 
+  {
+		return logar;
+  }
+  elif (x <= 0)
+  {
+		return -2;
+   }
+	else 
+  {
+		return -1;
+  }
+}
